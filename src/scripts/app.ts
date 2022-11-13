e3dc14f2da1d532ca3453b37bc20c99dea84b9f3
ivan-pl <pleskaiv@gmail.com>
2022-11-13 11:47:47 +0300

    style: add grid and style elements

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -13,20 +13,28 @@ function createLayout(): HTMLElement {
   const form = document.createElement("form");
   setAttributes(form, "message-box");
 
+  const h1 = document.createElement("h1");
+  h1.classList.add("title");
+  h1.innerText = "Chat";
+
   const buttonSend = document.createElement("button");
+  buttonSend.innerText = "Send";
   setAttributes(buttonSend, "send-message");
 
   const historySection = document.createElement("section");
   setAttributes(historySection, "message-history");
 
   const inputAuthor = document.createElement("input");
+  inputAuthor.setAttribute("type", "text");
+  inputAuthor.setAttribute("placeholder", "Your name");
   setAttributes(inputAuthor, "message-author");
 
   const messageEntryArea = document.createElement("textarea");
+  messageEntryArea.setAttribute("placeholder", "Message text");
   setAttributes(messageEntryArea, "message-entry-area");
 
-  [buttonSend, historySection, inputAuthor, messageEntryArea].forEach((el) =>
-    form.append(el)
+  [h1, buttonSend, historySection, inputAuthor, messageEntryArea].forEach(
+    (el) => form.append(el)
   );
 
   const app = document.createElement("div");
